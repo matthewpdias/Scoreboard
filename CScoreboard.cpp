6d2200251dbd7e86f4db50edff9a18e6066ec50a
matthewpdias <matthewpdias@gmail.com>
2016-11-09 13:35:26 -0800

    fixed and tested and done

--- CScoreboard.cpp
+++ CScoreboard.cpp
@@ -4,44 +4,57 @@
 
 using namespace std;
 
-CScoreboard::CScoreboard(int competitors, int periods){
-  assert(competitors > 1);
+CScoreboard::CScoreboard(int comp, int periods){
+  assert(comp > 1);
   assert(periods > 0);
   //for all of the periods
-  this->competitors = competitors;
+  competitors = comp;
   for(int i = 0; i < periods; i++){
     //loop over each competitor
+    scores.push_back(map<int, int>());
     for(int j = 0; j < competitors; j++){
       /*set all scores to zero
       i is the period // j is the competitor //score is zero
       scores[i] is a map
       scores[i][j] is a value for the jth key of that map */
-      (scores[i])[j] = 0;
+      scores[i][j] = 0;
     }
   }
 
 }
 
 void CScoreboard::setScore(int competitors, int period , int score){
-  (scores[period-1])[competitors] = score;
+  scores[period-1][competitors] = score;
 }
 
 int CScoreboard::getScore(int competitors, int period){
-  return (scores[period-1])[competitors];
+
+  if( competitors  > this->competitors || competitors < 1){
+    cout << "Competitor does not exists" << endl;
+    return 0;
+  }
+
+  return scores[period-1][competitors];
 }
 
 int CScoreboard::getTotalScore(int competitors){
+
+  if( competitors  > this->competitors || competitors < 1){
+    cout << "Competitor does not exists" << endl;
+    return 0;
+  }
+
   int Escore = 0;
   for(int i = 0; i < scores.size(); i++){
-    Escore += (scores[i])[competitors];
+    Escore += scores[i][competitors];
   }
   return Escore;
 }
 
 void CScoreboard::clearScoreboard(){
-  for(int i = 0; i < scores.size(); i++){
-    for(int j = 0; j < competitors; j++){
-      (scores[i])[j] = 0;
+  for(int i = 1; i <= competitors; i++){
+    for(int j = 1; j <= scores.size(); j++){
+      setScore(i, j, 0);
     }
   }
 }
--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ int main(int argc, char const *argv[]) {
 
   CScoreboard myScoreboard(2, 4);
 
+  myScoreboard.setScore(1, 1, 21);
+  myScoreboard.setScore(1, 2, 11);
+  myScoreboard.setScore(1, 3, 9);
+  myScoreboard.setScore(1, 4, 7);
+  myScoreboard.setScore(2, 1, 21);
+  myScoreboard.setScore(2, 2, 2);
+  myScoreboard.setScore(2, 3, 1);
+  myScoreboard.setScore(2, 4, 3);
+
   cout << myScoreboard.getScore(1, 1) << endl;
   cout << myScoreboard.getScore(1, 2) << endl;
   cout << myScoreboard.getScore(1, 3) << endl;
@@ -16,6 +25,15 @@ int main(int argc, char const *argv[]) {
   cout << myScoreboard.getScore(2, 2) << endl;
   cout << myScoreboard.getScore(2, 3) << endl;
   cout << myScoreboard.getScore(2, 4) << endl;
+
+  cout << myScoreboard.getTotalScore(1) << endl;
+  cout << "SHOULD ERROR" << endl;
+  cout << myScoreboard.getTotalScore(3) << endl;
+
+  myScoreboard.clearScoreboard();
+  cout << myScoreboard.getTotalScore(1) << endl;
+  cout << myScoreboard.getTotalScore(2) << endl;
+
 
   return 0;
 }
